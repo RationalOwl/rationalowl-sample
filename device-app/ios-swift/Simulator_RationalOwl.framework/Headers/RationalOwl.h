8e8ce16e781d06c950620802f5c611fef9063bf7
Rational Owl <jungdo@rationalowl.com>
2020-12-31 01:14:13 +0900

    add RationalOwl.framework for simulation
    1. xcode12.3 Universal(Fat) framework 버그로 인한 프레임워크 분할
     - 시뮬레이션용 및 IOS단말용 프레임워크를 별도로 분리
     - 개발환경에 기본 IOS단말용 프레임워크 설정됨
     - 시뮬레이션용 프레임워크는 Simulator_RationalOwl.framework 로 샘플앱 프로젝트내에 존재
     - 시뮬레이션용으로 구동시에는 프레임워크를 시뮬레이션용으로 변경하여 진행해야 함.
    
     ===== Eng description ================
    
    1. universal(fat) framework bug in xcdoe 12.3 version
     - seperate framework for device and simulator
     - default framework for device is set(RationalOwl.framework)
     - framework for simulator is Simulator_RationalOwl.framework
     - Simulator_RationalOwl.framework is located in the sample project
     - If You want to run sample app as simulation mode, set Simulator_RationalOwl.framework instead of default 'RationalOwl.framework'.

---
+++ device-app/ios-swift/Simulator_RationalOwl.framework/Headers/RationalOwl.h
@@ -0,0 +1,20 @@
+//
+//  RationalOwl.h
+//  RationalOwl
+//
+//  Created by 김정도 on 2016. 6. 28..
+//  Copyright © 2016년 RationalOwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RationalOwl.
+FOUNDATION_EXPORT double RationalOwlVersionNumber;
+
+//! Project version string for RationalOwl.
+FOUNDATION_EXPORT const unsigned char RationalOwlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RationalOwl/PublicHeader.h>
+#import <RationalOwl/MinervaManager.h>
+#import <RationalOwl/MinervaDelegate.h>
+#import <RationalOwl/Result.h>
